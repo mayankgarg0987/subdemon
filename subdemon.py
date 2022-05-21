380004f9fcf7417891eee3efbeb6ce6e9ca70134
Mayank Garg <55484263+mayankgarg0987@users.noreply.github.com>
2022-05-21 14:48:20 +0530

    Add files via upload

---
+++ subdemon.py
@@ -0,0 +1,92 @@
+import requests
+from threading import Thread, Lock
+from queue import Queue
+import colorama
+from colorama import Fore
+import socket
+
+
+
+
+q = Queue()
+list_lock = Lock()
+discovered_domains = []
+
+def banner():
+    bann = """
+           _         _                            
+ ___ _   _| |__   __| | ___ _ __ ___   ___  _ __  
+/ __| | | | '_ \ / _` |/ _ \ '_ ` _ \ / _ \| '_ \ 
+\__ \ |_| | |_) | (_| |  __/ | | | | | (_) | | | |
+|___/\__,_|_.__/ \__,_|\___|_| |_| |_|\___/|_| |_|
+
+               By- Mayank, Yash, Rishank, Shashank
+
+"""
+    print(Fore.RED + bann)
+
+def scan_subdomains(domain):
+    global q
+    while True:
+        
+        subdomain = q.get()
+        
+        url = f"https://{subdomain}.{domain}"
+        try:
+            requests.get(url)
+        except requests.ConnectionError:
+            pass
+        else:
+            
+            u = url[8:]
+            IP = socket.gethostbyname(u)
+            res = requests.get(url)
+            print(Fore.GREEN + "[+] Discovered subdomain: %-30s IP: %-30s STATUS CODE: %-30s" % (url, IP, res.status_code))  
+           
+            with list_lock:
+                discovered_domains.append(url)
+
+        
+        q.task_done()
+
+
+def main(domain, n_threads, subdomains):
+    global q
+
+    
+    for subdomain in subdomains:
+        q.put(subdomain)
+
+    for t in range(n_threads):
+        
+        worker = Thread(target=scan_subdomains, args=(domain,))
+        
+        worker.daemon = True
+        worker.start()
+
+
+if __name__ == "__main__":
+    import argparse
+    banner()
+    parser = argparse.ArgumentParser(description="Faster Subdomain Scanner using Threads")
+    parser.add_argument("domain", help="Domain to scan for subdomains without protocol (e.g without 'http://' or 'https://')")
+    parser.add_argument("-l", "--wordlist", help="File that contains all subdomains to scan, line by line. Default is subdomains.txt",
+                        default="subdomains.txt")
+    parser.add_argument("-t", "--num-threads", help="Number of threads to use to scan the domain. Default is 10", default=10, type=int)
+    parser.add_argument("-o", "--output-file", help="Specify the output text file to write discovered subdomains", default="discovered-subdomains.txt")
+    
+    args = parser.parse_args()
+    domain = args.domain
+    wordlist = args.wordlist
+    num_threads = args.num_threads
+    output_file = args.output_file
+
+    main(domain=domain, n_threads=num_threads, subdomains=open(wordlist).read().splitlines())
+    q.join()
+
+    
+    with open(output_file, "w") as f:
+        for url in discovered_domains:
+            print(url, file=f)
+    
+    print(Fore.WHITE)
